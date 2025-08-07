2d2c8c0dd11165998a342dd2a37871caa47921c1
Kashish <kashish.vaid29994@gmail.com>
2025-08-07 12:14:17 +0530

    added a side bar

---
+++ threading_streamlit_frontend.py
@@ -0,0 +1,45 @@
+import streamlit as st
+from backend import chatbot
+from langchain_core.messages import HumanMessage
+import uuid
+
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+#****************************Side bar UI************************
+
+st.sidebar.title("Langgraph Chatbot")
+
+st.sidebar.button("New Chat")
+st.sidebar.header("My Conversations")
+
+# **************************************** utility functions ************************* Add a dynamic threadid and it to sesssion*****
+
+
+
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+user_input = st.chat_input('Type here')
+
+if user_input:
+
+    # first add the message to message_history
+    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+
+    with st.chat_message('assistant'):
+
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream(
+                {'messages': [HumanMessage(content=user_input)]},
+                config= {'configurable': {'thread_id': 'thread-1'}},
+                stream_mode= 'messages'
+            )
+        )
+
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
